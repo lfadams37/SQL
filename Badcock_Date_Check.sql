48cac9bbd0a40963aa9ec6f0220848cf0cc2c833
lfadams37 <lfadams37@gmail.com>
2020-08-30 12:17:58 -0400

    Rename Date Check.sql to Badcock_Date_Check.sql

---
+++ Badcock_Date_Check.sql
@@ -0,0 +1,14 @@
+﻿SELECT TOP 1000 
+Date
+,PYDATE
+,SUM(QTYONHAND) as CY_QtyOnHand
+,SUM(pyqtyonhand) as PY_QtyOnHand
+,SUM(materialcost)as CY_MaterialCost
+,SUM(pymaterialcost) as PY_MaterialCost 
+
+FROM FinancialAnalysis.la.InventoryDash a
+
+--WHERE Date in ('2019-09-14','2018-09-14')
+
+GROUP BY Date,PYDATE
+ORDER BY Date desc
